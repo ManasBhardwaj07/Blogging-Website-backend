472cb7b51dd4a748772db8bbfad6d9698592ab48
Manas Bhardwaj <119048252+ManasBhardwaj07@users.noreply.github.com>
2024-07-24 23:20:05 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://soft-gecko-7059e8.netlify.app'
+    'https://wonderful-youtiao-ac091b.netlify.app'
 
 ];
 
