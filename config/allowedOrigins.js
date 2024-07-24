ca31d412f4300722d28998aa4367ca17c43bbd89
Manas Bhardwaj <119048252+ManasBhardwaj07@users.noreply.github.com>
2024-07-24 23:34:51 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://wonderful-youtiao-ac091b.netlify.app'
+    'https://funny-torte-ec0228.netlify.app'
 
 ];
 
